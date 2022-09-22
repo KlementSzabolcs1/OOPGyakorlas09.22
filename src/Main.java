9fdc26f39e6e7fd9fb75d81de80b89940c022ff1
KlementSzabolcs1 <klementszabolcs1@gmail.com>
2022-09-22 09:40:37 +0200

    Fájl beolvasása javitva

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Random;
 import java.util.Scanner;
 
 
@@ -7,15 +8,20 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner sc = new Scanner(new File("festmenyek.csv"));
         Festmeny[] festmeny = new Festmeny[1000];
+        int i = 0;
         while (sc.hasNext()) {
-            int i = 0;
             String[] egysor = sc.nextLine().split(";");
             festmeny[i].cim = egysor[0];
             festmeny[i].festo = egysor[1];
             festmeny[i].stilus = egysor[2];
-
             i++;
         }
         sc.close();
+        Random rnd = new Random();
+        i = 0;
+        while (i < 20) {
+
+            i++;
+        }
     }
 }
